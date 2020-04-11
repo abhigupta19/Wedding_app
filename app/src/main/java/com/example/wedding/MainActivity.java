c48ae99c8de9cdaf15b76807cd7894af54dc12bd
Ninaad <ninaad61348@gmail.com>
2020-04-11 23:14:25 +0530

    Picture click enabled

--- app/src/main/java/com/example/wedding/MainActivity.java
+++ app/src/main/java/com/example/wedding/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.wedding;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.view.View;
@@ -28,7 +33,10 @@ import com.google.firebase.storage.UploadTask;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,11 +57,14 @@ public class MainActivity extends AppCompatActivity {
     private final int PICK_IMAGE_REQUEST_FLIGHT = 72;
     Uri filepath_id;
     Uri file_path_flight;
+    Uri file_id;
+    Uri file_tkt;
     FirebaseStorage storage;
     StorageReference storageReference;
     String phone;
     FirebaseDatabase database;
-
+    Button id_click;
+    Button flight_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,13 +72,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         id_uploaded=false;
         flight_uploaded=false;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
         pnr_upload=false;
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
         nameText=findViewById(R.id.et_name);
         mailText=findViewById(R.id.et_email);
         id_up=findViewById(R.id.et_id_up);
-        pnr_submit=findViewById(R.id.et_pnr_up);
         fli_up=findViewById(R.id.et_flight_up);
         pnrText=findViewById(R.id.et_pnr_edit);
         idText=findViewById(R.id.id_txt);
@@ -76,6 +90,21 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=getIntent();
         phone= intent.getStringExtra("phone");
         database = FirebaseDatabase.getInstance();
+        id_click=findViewById(R.id.et_id_click);
+        flight_click=findViewById(R.id.et_flight_click);
+        id_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture("idClick");
+            }
+        });
+        flight_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture("tktClick");
+
+            }
+        });
 
         id_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
 
                 myRef.setValue("Hello, World!");
                 boolean is_ok=true;
+
                 if(nameText.getText().toString().isEmpty())
                 {
                     nameText.setError("Please Enter a name");
@@ -109,12 +139,12 @@ public class MainActivity extends AppCompatActivity {
                     mailText.setError("Please Enter your mail id");
                     is_ok=false;
                 }
-                if(!id_uploaded)
+                if(idText.getText().toString().isEmpty())
                 {
                     Toast.makeText(MainActivity.this, "Please Upload your ID Proof", Toast.LENGTH_SHORT).show();
                     is_ok=false;
                 }
-                if(!flight_uploaded && !pnr_upload)
+                if((flightText.getText().toString().isEmpty()) && (pnrText.getText().toString().isEmpty()))
                 {
                     is_ok=false;
                     Toast.makeText(MainActivity.this, "Please enter either pnr or flight ticket", Toast.LENGTH_SHORT).show();
@@ -129,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
                     String s_id=uploadImage(filepath_id);
                     String pnr=pnrText.getText().toString();
                     User user=new User(phone , name , s_id , s_flight , pnr);
+
                     myRef.setValue(user);
 
                 }
@@ -146,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
         {
             filepath_id = data.getData();
             idText.setText(getFileName(filepath_id));
-            id_uploaded=true;
 //                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
 //                imageView.setImageBitmap(bitmap);
 
@@ -157,11 +187,24 @@ public class MainActivity extends AppCompatActivity {
         {
             file_path_flight = data.getData();
             flightText.setText(getFileName(file_path_flight));
-            flight_uploaded=true;
 //                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
 //                imageView.setImageBitmap(bitmap);
 
 
+        }
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                idText.setText(getFileName(file_id));
+
+//                imageView.setImageURI(file);
+            }
+        }
+        if (requestCode == 101) {
+            if (resultCode == RESULT_OK) {
+//                imageView.setImageURI(file);
+                idText.setText(getFileName(file_tkt));
+
+            }
         }
     }
     private void chooseImage(String s) {
@@ -231,5 +274,29 @@ public class MainActivity extends AppCompatActivity {
         }
         else
             return "";
+    }
+    public void takePicture( String str) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+        if(str=="idClick")
+        startActivityForResult(intent, 100);
+        if(str=="tktClick")
+            startActivityForResult(intent, 101);
+
+    }
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
     }
 }
