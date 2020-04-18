4f1ec81dc7aca5bcfd0c3132665fdc03ba693fc8
Abhinav  Gupta <gabhi8740@gmail.com>
2020-04-18 13:28:17 +0530

    Bottom_sheet

--- app/src/main/java/com/example/wedding/MainActivity.java
+++ app/src/main/java/com/example/wedding/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.wedding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomAppBar bottomAppBar=findViewById(R.id.bottombar);
+        final BottomAppBar bottomAppBar=findViewById(R.id.bottombar);
         //bottomAppBar.setNavigationIcon(R.drawable.ic_home_black_24dp);
         //bottomAppBar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
         bottomAppBar.setPadding(20,10,0,0);
         bottomAppBar.setTitle("helo");
-        //setSupportActionBar(bottomAppBar);
+        //setSupportActionBar(bottomAppBar
+        FloatingActionButton floatingActionButton=findViewById(R.id.floatting);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Bottomsheet bottomsheet=new Bottomsheet();
+                    bottomsheet.show(getSupportFragmentManager(),"botoomsheet");
 
+            }
+        });
     }
 
 
